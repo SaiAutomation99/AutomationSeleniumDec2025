6546f33dfbe22331cf68fa02c4c1b900695f5176
Sai Prasanth <saiprasanth.qa@gmail.com>
2024-12-25 17:47:55 +0530

    Added the selenium jars and the invoke method for chrome

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,6 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -8,6 +11,14 @@ public class Test {
     public static void m1(){
 System.out.println("hello m1");
         System.out.println("hello m1");
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.co.in/");
+        driver.manage().window().maximize();
+        //driver.getTitle();
+        System.out.println(driver.getTitle());
+        driver.manage().deleteAllCookies();
+        driver.quit();
 
     }
 
