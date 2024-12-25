5b1906eafaf43235ef3326e5feb0e20e2fcfbbbe
Sai Prasanth <saiprasanth.qa@gmail.com>
2024-12-25 17:01:06 +0530

    "added classes"

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        m1();
+    }
+
+    public static void m1(){
+System.out.println("hello m1");
+
+    }
+    
+}
