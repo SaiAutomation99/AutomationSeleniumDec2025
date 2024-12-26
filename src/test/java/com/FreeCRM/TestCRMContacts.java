4ba7b7756bdd94cd0676ad58082014f9c97818a2
Sai Prasanth <saiprasanth.qa@gmail.com>
2024-12-26 14:03:17 +0530

    added the code for calendar

--- src/test/java/com/FreeCRM/TestCRMContacts.java
+++ src/test/java/com/FreeCRM/TestCRMContacts.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import util.Encoder_Decorder;
 import util.Reusable;
@@ -33,9 +34,30 @@ public class TestCRMContacts {
 	      reusable.enterTest(username, email);
 	      String pass="MTIzNDU2Nzg5";
 	      String passcode=  decoder.deCoderInput(pass);
-	      reusable.enterTest(username, passcode);
-	      
+	      WebElement password=driver.findElement(By.xpath("//input[@name='password']"));
+	      reusable.enterTest(password, passcode);
+	      driver.findElement(By.xpath("//div[text()='Login']")).click();
 	      System.out.println(driver.getTitle());
+	      
+//	      WebElement span=driver.findElement(By.xpath("//span[text()='Contacts']/parent::a/i"));
+	      Actions action=new Actions(driver);
+//	    
+//	      action.moveToElement(driver.findElement(By.xpath("//span[text()='Contacts']/parent::a/i"))).build().perform();
+//	      span.click();
+//	     WebElement contacts=driver.findElement(By.xpath("//span[text()='Contacts']/parent::a/span")); 
+//	      action.moveToElement(contacts).build().perform();
+//	      contacts.click();
+	      String name="s s";
+	      driver.findElement(By.xpath("//div[text()='Showing 4 records']")).click();
+	     WebElement contactslist= driver.findElement(By.xpath("(//a[text()='"+name+"'])[2]/parent::td/preceding-sibling::td//div"));
+	      action.moveToElement(contactslist).build().perform();
+	      contactslist.click();
+	      try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// TODO: handle exception
+			//(//a[text()='sai prasanth'])[1]/parent::td/preceding-sibling::td//input
+		}
 	      driver.quit();
 	      
 	      
